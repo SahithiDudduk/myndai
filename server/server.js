d7d2b82373d9f3cb2b60ada2b9a0c937128c899c
duddukuri <duddukuri@mapmyindia.com>
2024-09-30 22:45:22 +0530

    Serve static files and update CORS

--- server/server.js
+++ server/server.js
@@ -2,27 +2,44 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const userRoutes = require('./routes/userRoutes'); // Ensure this path is correct
-const helmet = require('helmet'); // For security
-const morgan = require('morgan'); // For logging
-require('dotenv').config(); // To load environment variables
+const userRoutes = require('./routes/userRoutes');
+const path = require('path');
+require('dotenv').config(); // Load environment variables
 
 const app = express();
 const port = process.env.PORT || 5000; // Use PORT from environment or default to 5000
 
 // Middleware
-app.use(cors()); // Enable CORS for all origins
-app.use(helmet()); // Adds various security headers
-app.use(morgan('combined')); // Logs requests to the console
+const allowedOrigins = [
+  'http://localhost:3000', // Local development
+  'https://myndai-g33ynvfrb-sahithis-projects-cca48538.vercel.app',
+  'https://myapp.herokuapp.com', // Heroku app
+  // Vercel deployment
+];
+
+app.use(cors({
+  origin: allowedOrigins,
+})); // Enable CORS for specified origins
 app.use(bodyParser.json()); // Parse JSON bodies
 
 // Use routes
-app.use('/api', userRoutes); // Ensure this path is correct
+app.use('/api', userRoutes);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected!'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// The "catchall" handler
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 // Start server
 app.listen(port, () => {
